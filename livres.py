f0f034e0fbc53276259553bb5ba364152aafa4c4
Jean-Simon Cyr <jsimcyr@gmail.com>
2025-12-10 16:19:24 -0500

    Ajout de la fonction ajouter_livre

--- livres.py
+++ livres.py
@@ -2,7 +2,23 @@
 bibliotheque = []
 
 def ajouter_livre(titre : str, auteur : str):
-    pass
+    '''
+    Docstring for ajouter_livre
+    
+    :param titre: Le titre du livre que vous désirez ajouter
+    :type titre: str
+    :param auteur: L'auteur de ce livre
+    :type auteur: str
+    '''
+    # Vérifie qu'il n'y ait pas deux fois le même livre
+    for livre in bibliotheque:
+        if livre ["titre"] == titre:
+            print("Ce livre est déjà dans votre bibliothèque")
+            return
+        
+    livre = {"titre" : titre, "auteur" : auteur}
+    bibliotheque.append(livre)
+    print(f"Le livre {titre} a été ajouté avec succès !")
 
 def afficher_livres():
     pass
